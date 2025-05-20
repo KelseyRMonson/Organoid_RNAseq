ee88d263c8050d6f608845d28f3b370963abaed9
Kelsey Monson <kelsey.monson@mssm.edu>
2025-05-20 15:26:45 -0400

    Perform library normalization

--- Pilot_20MAY25/EdgeR/KS_Organoid_Pilot_edgerR_Script.R
+++ Pilot_20MAY25/EdgeR/KS_Organoid_Pilot_edgerR_Script.R
@@ -12,31 +12,51 @@ pacman::p_load(here,
                ggrepel) # Optimize plot label separation 
 library(GseaVis) # For nicer GSEA plot
 
-# This is the script for differential expression for the pilot experiment from Konrad's mouse organoids
+# This is the initial data cleaning and normalization script for differential expression for the pilot experiment from Konrad's mouse organoids
 
 # Read in the raw data and metadata ----
-raw.data <- read.delim("salmon.merged.gene_counts.tsv", check.names=FALSE, stringsAsFactors=FALSE,row.names="gene_id")
-raw.data <- read.delim("salmon.merged.gene_counts.tsv", check.names=FALSE, stringsAsFactors=FALSE,row.names="gene_id")
-# Metadata
-metadata <- read.delim("lenvatinib_metadata.txt",check.names=FALSE, stringsAsFactors=FALSE)
-metadata$Treatment <- factor(metadata$Treatment, levels=c("No","Yes")) # Simple 2v2 test
+## Raw data
+raw.data <- read.delim("../results/star_salmon/salmon.merged.gene_counts.tsv", check.names=FALSE, stringsAsFactors=FALSE,row.names="gene_id")
+## Metadata
+metadata <- read.delim("organoid_pilot_metadata.txt",check.names=FALSE, stringsAsFactors=FALSE)
+### View the metadata
+metadata
+### Make "Condition" (wildtype or mutant) a factor
+metadata$Condition <- factor(metadata$Condition, levels=c("WT","Mut"))
+### View the metadata
+str(metadata)
+table(metadata)
+#### CAUTION 
+#### edgeR metadata is assigned by the order of the rows in the raw gene expression table
+#### This is fine for us because we want WT to be the reference, and WT was listed first
+#### We need to be cautious that we are setting the appropriate reference
 
-## Because it seems like the metadata is assigned based off of the order of the rows in the raw gene expression table, this is my workaround for now (not really feasible for larger datasets)
-raw.data <- raw.data[c("gene_name","N.C","N.100","T.C","T.100")]
 
 # Create edgeR list object (called DGEList)
-y <- DGEList(counts=raw.data,group=metadata$Treatment)
+y <- DGEList(counts=raw.data,group=metadata$Condition)
+## View our edgeR object
+str(y)
 
-# Filtering out lowly expressed genes and re-calculating library size after filtering
+# Filter out lowly expressed genes
 keep <- filterByExpr(y)
+## This is a logical vector
+## We can see the number of genes kept by counting how many are `TRUE`
+## And how many dropped by counting `FALSE`
+summary(keep)
+
+# Re-calculate library size after filtering
 y <- y[keep,,keep.lib.sizes=FALSE]
+## Confirm that the genes we wanted to keep are kept
+## You can do this by comparing the length of the `gene_name` variable matches the `TRUE` count from above
+str(y)
 
 # Normalization 
-# Calculates a scaling factor (trimmed mean of M-values, TMM)
-# Multiplies this normalization factor by the original library size to get the effective (normalized) library size used for downstream analysis
-# norm.factor < 1 = small number of high-count genes are monopolizing the library, library size is scaled down/counts are scaled up
-# norm.factor > 1 = library size scaled up/counts scaled down 
+## Calculates a scaling factor (trimmed mean of M-values, TMM)
+## Multiplies this normalization factor by the original library size to get the effective (normalized) library size used for downstream analysis
+## norm.factor < 1 = small number of high-count genes are monopolizing the library, library size is scaled down/counts are scaled up
+## norm.factor > 1 = library size scaled up/counts scaled down 
 y <- normLibSizes(y)
+## You can see the norm.factors below:
 y$samples
 
 # Setting model design, defining treatment as the covariate of interest 
