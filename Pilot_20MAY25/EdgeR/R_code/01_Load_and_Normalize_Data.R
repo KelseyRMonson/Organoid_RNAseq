f73e1950a4a3904252405d6f066f488d8ed70468
Kelsey Monson <kelsey.monson@mssm.edu>
2025-05-20 17:45:41 -0400

    Use stricter filtering threshold for normalization

--- Pilot_20MAY25/EdgeR/R_code/01_Load_and_Normalize_Data.R
+++ Pilot_20MAY25/EdgeR/R_code/01_Load_and_Normalize_Data.R
@@ -2,13 +2,15 @@
 # Written by KM on 20MAY25
 
 # First load packages ----
+# These are all from Bioconductor
 library(edgeR) # For differential expression calculation. We will create an edgeR object and use the package to normalize our data.
+library(AnnotationDbi) # To annotate with Entrez IDs
+library(org.Mm.eg.db) # To annotate with Mouse Entrez IDs
 
 # Read in the raw data and metadata ----
 ## Raw data
 raw.data <- read.delim("../results/star_salmon/salmon.merged.gene_counts.tsv", check.names=FALSE, stringsAsFactors=FALSE)
-### View the raw data
-view(raw.data)
+### View the raw data structure
 str(raw.data)
 
 ## Metadata
@@ -25,53 +27,40 @@ table(metadata)
 #### This is fine for us because we want WT to be the reference, and WT was listed first
 #### We need to be cautious that we are setting the appropriate reference
 
+# Create edgeR DGEList Object ----
+y <- DGEList(counts=raw.data,group=metadata$Condition)
+## View our edgeR object
+str(y)
 
-# Setting model design, defining condition as the covariate of interest
-## Defining the grouping variable (condition)
-group <- factor(c("WT","Mut"))
-
-design <- model.matrix(~group)
-design.reduced <- matrix(1,2,1)
-dgList <- estimateGLMCommonDisp(data, design.reduced,
-                                method="deviance", robust = TRUE, subset=NULL)
-
-
-
-
-# Annotating with ENTREZ IDs to do GO and pathway enrichment
+# Annotating with ENTREZ IDs to do GO and pathway enrichment ----
+# This is kind of complex, but to do pathway enrichment we need to add other gene IDs (called Entrez IDs)
 
 # Need ENTREZ ID to be the row name, but cannot have duplicate row names (some genes have unique Ensembl IDs but not gene names/Entrez IDs)
 # Finding the Ensembl IDs currently in the database 
-idfound<-dgList$genes$gene_id %in% mappedRkeys(org.Mm.egENSEMBL)
-dgList<-dgList[idfound,]
-dim(dgList)
+idfound<-y$genes$gene_id %in% mappedRkeys(org.Mm.egENSEMBL)
+y<-y[idfound,]
+dim(y)
 
 # Then adding ENTREZ ID
 egENSEMBL<-toTable(org.Mm.egENSEMBL)
 head(egENSEMBL)
 
-m<-match(dgList$genes$gene_id, egENSEMBL$ensembl_id)
-dgList$genes$EntrezGene<-egENSEMBL$gene_id[m]
-head(dgList$genes)
+m<-match(y$genes$gene_id, egENSEMBL$ensembl_id)
+y$genes$EntrezGene<-egENSEMBL$gene_id[m]
+head(y$genes)
 
 # Dropping duplcated genes (both gene names and Entrez IDs)
-o <- order(rowSums(dgList$counts), decreasing=TRUE)
-dgList <- dgList[o,]
-d <- duplicated(dgList$genes$gene_name)
-dgList.unique <- dgList[!d,]
-e <- duplicated(dgList.unique$genes$EntrezGene)
-dgList.unique <- dgList.unique[!e,]
-nrow(dgList.unique)
+o <- order(rowSums(y$counts), decreasing=TRUE)
+y <- y[o,]
+d <- duplicated(y$genes$gene_name)
+y.unique <- y[!d,]
+e <- duplicated(y.unique$genes$EntrezGene)
+y.unique <- y.unique[!e,]
+nrow(y.unique)
 
 # Making the Entrez ID the row name (and dropping duplicative data from EntrezGene column)
-rownames(dgList.unique$counts) <- rownames(dgList.unique$genes) <- dgList.unique$genes$EntrezGene
-dgList.unique$genes$EntrezGene <- NULL
-
-# Create edgeR Object ----
-# Create edgeR list object (called DGEList)
-y <- DGEList(counts=raw.data,group=metadata$Condition)
-## View our edgeR object
-str(y)
+rownames(y.unique$counts) <- rownames(y.unique$genes) <- y.unique$genes$EntrezGene
+y.unique$genes$EntrezGene <- NULL
 
 # Filter out lowly expressed genes ----
 keep <- filterByExpr(y)
@@ -80,8 +69,15 @@ keep <- filterByExpr(y)
 ## And how many dropped by counting `FALSE`
 summary(keep)
 
+# To filter out some noise, we are going to set a stricter filtering threshold
+keep2 <- rowSums( cpm(y) > 0.5 ) >=2
+summary(keep2)
+
+# We will take the `keep2` filtering threshold going forward
+
+
 # Re-calculate library size after filtering
-y <- y[keep,,keep.lib.sizes=FALSE]
+y <- y[keep2,,keep.lib.sizes=FALSE]
 ## Confirm that the genes we wanted to keep are kept
 ## You can do this by comparing the length of the `gene_name` variable matches the `TRUE` count from above
 str(y)
@@ -95,6 +91,12 @@ y <- normLibSizes(y)
 ## You can see the norm.factors below:
 y$samples
 
+
+
 # Save the data ----
 ## Save the normalized data in RDS format
 saveRDS(y, file = "input/normalized_data.rds")
+
+
+
+
